7f4e381a5ec665ccf4721bae0532b372df161808
srinidhim2 <shrinidhi3398@gmail.com>
2025-06-25 16:39:48 +0530

    intial commit : ts and prisma integration working with node

---
+++ script.ts
@@ -0,0 +1,33 @@
+import { userInfo } from 'os';
+import express from 'express';
+import { PrismaClient, Prisma} from '@prisma/client';
+import morgan from 'morgan'
+
+
+const prisma = new PrismaClient()
+
+// const app = express();
+// app.use(morgan('dev')); 
+
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Hello, world!' });
+// });
+
+// app.listen(3000, () => console.log('Server is running'));
+
+async function createUser(user: Prisma.UserCreateInput) {
+    const newUser = await prisma.user.create({ data: user });
+    return newUser;
+}
+
+async function main() {
+    const userInfo = {name:"raju"}
+    const user = await createUser(userInfo)
+    console.log(user)
+}
+
+main()
+.catch(e=>console.log(e.message))
+.finally(async ()=>
+    prisma.$disconnect()
+)
